3a14b0ec93861ca8ba55c021d4bced92e9c6df43
Gurveen-Singhm <gurveensingh45@gmail.com>
2021-08-12 00:51:28 +0530

    KEY Updation

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const KEY = "AIzaSyADAFShn-_PMjhEJPq0nNpCEStbQHOKiN4";
+const KEY1 = "AIzaSyADAFShn-";
+const KEY2 = "_PMjhEJPq0nNpCEStbQHOKiN4";
+const KEY = KEY1 + KEY2;
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
