059c71e07079efe0fbe78c5b4690191d67eea86c
Gurveen-Singhm <gurveensingh45@gmail.com>
2021-08-22 00:44:34 +0530

    Converted Class component to Function Component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./components/SearchBar";
 import VideoList from "./components/VideoList";
 import VideoDetail from "./components/VideoDetail";
 import YouTube from "./apis/YouTube";
-class App extends React.Component {
-  componentDidMount() {
-    this.onSearchSubmit("Tanay Pratap");
-  }
-  state = { videoData: [], selectedVideo: null };
 
-  async onSearchSubmit(term) {
+const App = () => {
+  const [videoData, setVideoData] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  useEffect(() => {
+    onSearchSubmit("Tanay Pratap");
+  }, []);
+
+  const onSearchSubmit = async (term) => {
     const response = await YouTube.get("/search", { params: { q: term } });
-    this.setState({
-      videoData: response.data.items,
-      selectedVideo: response.data.items[1],
-    });
-  }
 
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
+    setVideoData(response.data.items);
+    setSelectedVideo(response.data.items[1]);
   };
 
-  render() {
-    return (
-      <div style={{ padding: "10px 10px", backgroundColor: "#000" }}>
-        <SearchBar onFormSubmit={this.onSearchSubmit.bind(this)} />
-        <div className="ui grid">
-          <div className=" ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                videos={this.state.videoData}
-                onVideoSelect={this.onVideoSelect}
-              />
-            </div>
+  return (
+    <div style={{ padding: "10px 10px", backgroundColor: "#000" }}>
+      <SearchBar onFormSubmit={onSearchSubmit} />
+      <div className="ui grid">
+        <div className=" ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList videos={videoData} onVideoSelect={setSelectedVideo} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,38 +1,33 @@
-import React from "react";
-class SearchBar extends React.Component {
-  state = { term: "" };
+import React, { useState } from "react";
 
-  onChangeInput(e) {
-    this.setState({ term: e.target.value });
-  }
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
 
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.onFormSubmit(this.state.term);
-  }
+    onFormSubmit(term);
+  };
 
-  render() {
-    return (
-      <div
-        className="search-bar ui segment"
-        style={{ backgroundColor: " #ff0000" }}
-      >
-        <form onSubmit={this.handleSubmit.bind(this)} className="ui form">
-          <div className="field">
-            <label style={{ fontWeight: "900", padding: "5px" }}>
-              {" "}
-              Search Videos
-            </label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onChangeInput.bind(this)}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      className="search-bar ui segment"
+      style={{ backgroundColor: " #ff0000" }}
+    >
+      <form onSubmit={handleSubmit} className="ui form">
+        <div className="field">
+          <label style={{ fontWeight: "900", padding: "5px" }}>
+            {" "}
+            Search Videos
+          </label>
+          <input
+            type="text"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
